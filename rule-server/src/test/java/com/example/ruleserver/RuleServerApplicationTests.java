36399db0e245f9de35199c7a5875a4c057d721c5
mungmang <35050941+mumgmangmange@users.noreply.github.com>
2022-05-02 19:07:11 +0900

    Add files via upload

---
+++ rule-server/src/test/java/com/example/ruleserver/RuleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ruleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
